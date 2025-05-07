b4260ac7af4f93e379ef9da3a171119f6d8c5853
Kingsley <blooketmain@gmail.com>
2025-05-07 17:58:59 +0800

    made green background and made chicken unhided

--- game.js
+++ game.js
@@ -1,59 +1,54 @@
 document.addEventListener("DOMContentLoaded", () => {
     var canvas = document.getElementById("canvas");
     var ctx = canvas.getContext("2d");
-
+  
+    var frogX = 325;
+    var frogY = 225;
+  
+    document.addEventListener("keydown", move, false);
+  
     var rightPressed = false;
     var leftPressed = false;
     var upPressed = false;
     var downPressed = false;
 
-    var chickenX = 50;
-    var chickenY = 200;
-
-    document.addEventListener("keydown", move, false);
     function move(event) {
-
-        if (event.key == "ArrowRight") {
-            rightPressed = true;
-            leftPressed = false;
-            upPressed = false;
-            downPressed = false;
-            console.log("Moving Right");
-          } else if (event.key == "ArrowLeft") {
-            rightPressed = false;
-            leftPressed = true;
-            upPressed = false;
-            downPressed = false;
-            console.log("Moving Left");
-          } else if (event.key == "ArrowUp") {
-            rightPressed = false;
-            leftPressed = false;
-            upPressed = true;
-            downPressed = false;
-            console.log("Moving Up");
-          } else if (event.key == "ArrowDown") {
-            rightPressed = false;
-            leftPressed = false;
-            upPressed = false;
-            downPressed = true;
-            console.log("Moving Down");
-          }
-
-    }
-
-    function drawChicken() {
-        var chicken = document.getElementById("chicken-image");
-        ctx.drawImage(chicken, chickenX, chickenY, 50, 50);
-        console.log(drawChicken)
-    }
-
-    function draw() {
+        console.log(event);
+    
+        if (event.code == "ArrowRight") {
+          rightPressed = true;
+          leftPressed = false;
+          upPressed = false;
+          downPressed = false;
+        } else if (event.code == "ArrowLeft") {
+          leftPressed = true;
+          rightPressed = false;
+          upPressed = false;
+          downPressed = false;
+    } else if (event.code == "ArrowDown") {
+          downPressed = true;
+          leftPressed = false;
+          rightPressed = false;
+          upPressed = false;
+        } else if (event.code == "ArrowUp") {
+          upPressed = true;
+          leftPressed = false;
+          rightPressed = false;
+          downPressed = false;
+        }
+      }
+      function drawChicken() {
+        var img = document.getElementById("chicken-image");
+        ctx.drawImage(img, frogX, frogY, 50, 50);
+      }
+    
+      function draw() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
         ctx.beginPath();
         drawChicken();
         ctx.closePath();
-    }
-
-    setInterval(draw, 500);
-
-});
+      }
+    
+      setInterval(draw, 500);
+    });
